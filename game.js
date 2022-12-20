67d142d6417b37abdd249c4249441f74197a6ff8
Daniel Figueroa <da.figuepa@gmail.com>
2022-12-19 20:02:23 -0500

    ¿Qué es canvas en JavaScript? class

---
+++ game.js
@@ -0,0 +1,16 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d'); // canvas need a context to use it. Could be 2d or 3d
+
+window.addEventListener('load', startGame); // You can use window 
+
+function startGame() {
+  // Methods to use with canvas
+  // game.fillRect(0,50,100,100);
+  // game.clearRect(50,50,50,50);
+  // game.clear();
+
+  // game.font = '25px Verdana';
+  // game.fillStyle = 'purple';
+  // game.textAlign = 'center';
+  // game.fillText('Platzi', 25, 25);
+}
