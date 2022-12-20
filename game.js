1de03cb6dbd9ae182aa4d12bf6d365c11c63a31c
Daniel Figueroa <68354185+danielFiguePa@users.noreply.github.com>
2022-12-20 11:35:23 -0500

    Tamaño del canvas y sus elementos class

--- game.js
+++ game.js
@@ -4,6 +4,29 @@ const game = canvas.getContext('2d'); // canvas need a context to use it. Could 
 window.addEventListener('load', startGame); // You can use window 
 
 function startGame() {
+  let canvasSize;
+
+  if(window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+
+  const elementSize = canvasSize / 10;
+
+  game.font = elementSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for(let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementSize, elementSize * i);
+  }
+
+
+
+
   // Methods to use with canvas
   // game.fillRect(0,50,100,100);
   // game.clearRect(50,50,50,50);
